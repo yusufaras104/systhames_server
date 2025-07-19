8263d95f52a9cc04aeaba918789ae609f3ef95c5
Yusuf Aras <yusufenessara@gmail.com>
2025-07-19 16:23:11 +0100

    message

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'Systhames Networking Ltd.',
 
-  projectId: process.env.SANITY_PROJECT_ID || 'vx4ywdhc',
-  dataset: process.env.SANITY_DATASET || 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'vx4ywdhc',
+  dataset: process.env.SANITY_STUDIO_DATASET || 'production',
 
   plugins: [deskTool(), visionTool()],
 
